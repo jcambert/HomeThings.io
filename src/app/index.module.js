bdad2f2665dbe241fbf3d6bd245da1dafa17ba2d
jcambert <jc.ambert@free.fr>
2016-05-31 21:54:49 +0200

    First Commit

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('homeThingsIo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap','rx', 'toastr','frapontillo.bootstrap-switch','gridstack-angular','ngGauge'])
+    .module('homeThingsIo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap','rx', 'toastr','frapontillo.bootstrap-switch','gridster','ngGauge'])
    
     ;
 
